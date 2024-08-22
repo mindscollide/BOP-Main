cd1c0903989e66bf5876257f129c8364a621d440
Owais Wajid Khan <owaiswajidkhan@gmail.com>
2024-08-22 12:39:19 +0500

    App

--- src/components/common/tabs/Tabs.jsx
+++ src/components/common/tabs/Tabs.jsx
@@ -4,6 +4,18 @@ import Tabs from 'react-bootstrap/Tabs';
 import "./Tabs.css"
 
 const GlobalTabs = ({ tabs, defaultActiveKey = '0' }) => {
+
+    // This component will be passed as
+    // <GlobalTabs tabs={tabsData} defaultActiveKey={index of the tab} />
+
+    // const tabsData = [
+    //     { title: 'Live Rates', content: <div>Tab content for Home</div> },
+    //     { title: 'Forwards', content: <div>Tab content for Profile</div> },
+    //     { title: 'Discounting', content: <div>Tab content for Contact</div> }
+    // ];
+
+
+
     return (
         <Tabs
             defaultActiveKey={defaultActiveKey}
