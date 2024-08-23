71bc16c552ab4d37c00ad6824dfacfab6840b473
Ali Raza Mamdani <Ali.mamdani@hotmail.com>
2024-08-23 11:37:31 +0500

    code push

--- src/components/features/forwardsForTreasuryAndBranch/ForwardsForTreasuryAndBranch.jsx
+++ src/components/features/forwardsForTreasuryAndBranch/ForwardsForTreasuryAndBranch.jsx
@@ -3,6 +3,7 @@ import { Row, Col } from "react-bootstrap";
 import CustomButton from "../../common/globalButton/button";
 import SelectDropdown from "../../common/selectDropdown/SelectDropdown";
 import IconElement from "../../common/IconElement/IconElement";
+import ForwardsForTreasuryAndBranchTable from "../forwardsForTreasuryAndBranchTable/ForwardsForTreasuryAndBranchTable";
 
 const ForwardsForTreasuryAndBranch = () => {
   return (
@@ -16,22 +17,26 @@ const ForwardsForTreasuryAndBranch = () => {
         <Col sm={12} md={6} lg={6} className='flex-fill text-end'>
           <CustomButton value={"Create Tenor"} applyClass='createTenorBtn' />
         </Col>
-        <div
-          class='d-flex select-br-days flex-wrap justify-content-center'
-          data-select2-id='6'>
-          <div class='w-fix-350  '>
-            <div className='input-group'>
-              <SelectDropdown classNamePrefix={"DealerDropDown"} />
-              <CustomButton
-              
-                value={"Add"}
-                iconPosition={"start"}
-                applyClass='PlusButton'
-                icon={<IconElement iconClass={"icon-add-circle-fill fs-4"} />}
-              />
+        <Col sm={12} md={12} lg={12}>
+          <div
+            class='d-flex select-br-days flex-wrap justify-content-center'
+            data-select2-id='6'>
+            <div class='w-fix-350  '>
+              <div className='input-group'>
+                <SelectDropdown classNamePrefix={"DealerDropDown"} />
+                <CustomButton
+                  value={"Add"}
+                  iconPosition={"start"}
+                  applyClass='PlusButton'
+                  icon={<IconElement iconClass={"icon-add-circle-fill fs-4"} />}
+                />
+              </div>
             </div>
           </div>
-        </div>
+        </Col>
+        <Col sm={12} md={12} lg={12} className="mt-3">
+          <ForwardsForTreasuryAndBranchTable />
+        </Col>
       </Row>
       {/* <Row className='mt-4 mb-2'>
         <Col sm={12} md={6} lg={6} className="d-flex justify-content-center">
---
+++ src/components/features/forwardsForTreasuryAndBranchTable/ForwardsForTreasuryAndBranchTable.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import GlobalTable from "../../common/table/GlobalTable";
+import InputFIeld from "../../common/inputField/InputField";
+import CustomButton from "../../common/globalButton/button";
+import IconElement from "../../common/IconElement/IconElement";
+InputFIeld;
+const ForwardsForTreasuryAndBranchTable = () => {
+  const dataSource = [
+    { key: "1", tenor: "O N" },
+    { key: "2", tenor: "1 WEEK" },
+    { key: "3", tenor: "2 WEEK" },
+    { key: "4", tenor: "1 MONTH" },
+    { key: "5", tenor: "2 MONTH" },
+    { key: "6", tenor: "3 MONTH" },
+    { key: "7", tenor: "4 MONTH" },
+    { key: "8", tenor: "5 MONTH" },
+    { key: "9", tenor: "6 MONTH" },
+    { key: "10", tenor: "7 MONTH" },
+    { key: "11", tenor: "8 MONTH" },
+    { key: "12", tenor: "9 MONTH" },
+    { key: "13", tenor: "10 MONTH" },
+    { key: "14", tenor: "11 MONTH" },
+    { key: "15", tenor: "1 YEAR" },
+    { key: "16", tenor: "8 DAY" },
+  ];
+
+  const columns = [
+    {
+      title: "",
+      children: [
+        {
+          title: "Tenor",
+          dataIndex: "tenor",
+          key: "tenor",
+          width: 250,
+          render: (text) => {
+            return <span className="ms-2">{text}</span>
+          }
+        },
+      ],
+    },
+    {
+      title: "Current",
+      children: [
+        {
+          title: "Bid",
+          dataIndex: "currentBid",
+          key: "currentBid",
+          align: "center",
+          render: () => <InputFIeld type="number" applyClass={"DealerTableBitInput"}/>,
+        },
+        {
+          title: "Ask",
+          dataIndex: "currentAsk",
+          key: "currentAsk",
+          align: "center",
+
+          render: () => <InputFIeld type="number" applyClass={"DealerTableBitInput"} />,
+        },
+      ],
+    },
+    {
+      title: "Last",
+      children: [
+        {
+          title: "Bid",
+          dataIndex: "lastBid",
+          key: "lastBid",
+          align: "center",
+
+          render: () => <InputFIeld type="number" applyClass={"DealerTableBitInput"} />,
+        },
+        {
+          title: "Ask",
+          dataIndex: "lastAsk",
+          key: "lastAsk",
+          align: "center",
+
+          render: () => <InputFIeld type="number" applyClass={"DealerTableBitInput"} />,
+        },
+      ],
+    },
+    {
+      title: "",
+      key: "",
+
+      children: [
+        {
+          title: "",
+          dataIndex: "",
+          key: "",
+          width: 80,
+          align: "center",
+          render: () => {
+            return (
+              <CustomButton
+                type='link'
+                icon={
+                  <IconElement
+                    iconClass={"icon-trash color-red fs-6 cursor-pointer"}
+                  />
+                }
+              />
+            );
+          },
+        },
+      ],
+    },
+  ];
+
+  return (
+    <GlobalTable
+      columns={columns}
+      dataSource={dataSource}
+      prefixCls={"ForwardsForTreasuryAndBranchTable"}
+      // bordered
+      pagination={false}
+    />
+  );
+};
+
+export default ForwardsForTreasuryAndBranchTable;
