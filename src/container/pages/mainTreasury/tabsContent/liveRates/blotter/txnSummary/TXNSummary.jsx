675fbf953b58a7f68e04d9b112c795a8062cbd9c
Owais Wajid Khan <owaiswajidkhan@gmail.com>
2024-08-27 19:25:23 +0500

    code push

--- src/container/pages/mainTreasury/tabsContent/liveRates/blotter/txnSummary/TXNSummary.jsx
+++ src/container/pages/mainTreasury/tabsContent/liveRates/blotter/txnSummary/TXNSummary.jsx
@@ -3,88 +3,161 @@ import GlobalTable from '../../../../../../../components/common/table/GlobalTabl
 
 const TXNSummary = () => {
 
+    const tableData = [
+        {
+            key: '1',
+            txnID: '27-08-2024/a2fe',
+            client: 'Test',
+            side: 'Buy',
+            nature: '6',
+            ccy1: 'USD',
+            amount: '123',
+            rate: '288.00',
+            ccy2: 'PKR',
+            amount2: '',
+            time: '12:20 pm',
+            lcNo: '2131231',
+            accountNo: '123123',
+            comment: '',
+            status: 'Accepted',
+            chat: 21
+        },
+        {
+            key: '2',
+            txnID: '27-08-2024/a2fe',
+            client: 'Test',
+            side: 'Buy',
+            nature: '6',
+            ccy1: 'USD',
+            amount: '123',
+            rate: '288.00',
+            ccy2: 'PKR',
+            amount2: '',
+            time: '12:20 pm',
+            lcNo: '2131231',
+            accountNo: '123123',
+            comment: 'Test Comment',
+            status: 'Rejected',
+            chat: 21
+        },
+        {
+            key: "3",
+            txnID: "27-08-2024/9836",
+            client: "Syed Muhammad Aun Naqvi",
+            side: "Sell",
+            nature: "1",
+            ccy1: "USD",
+            amount: "999,999",
+            rate: "289.00",
+            ccy2: "PKR",
+            amount2: "",
+            time: "19:01 pm",
+            lcNo: "3142fdasfasd34214312412341234123412341234",
+            accountNo: "4124141241241241241241412412412412412",
+            comment: "",
+            status: "Accepted",
+            chat: 0
+        }
+
+    ]
+
     const columns = [
         {
             key: "1",
             title: "TXN ID",
+            dataIndex: 'txnID',
             className: "ff-poppins fw-bold",
-            width: "113px"
         },
         {
             key: "2",
             title: "Client",
-            className: "ff-poppins fw-bold"
+            dataIndex: 'client',
+            className: "ff-poppins fw-bold",
         },
         {
             key: "3",
             title: "Side",
-            className: "ff-poppins fw-bold"
+            dataIndex: 'side',
+            className: "ff-poppins fw-bold",
         },
         {
             key: "4",
             title: "Nature",
-            className: "ff-poppins fw-bold"
+            dataIndex: 'nature',
+            className: "ff-poppins fw-bold",
         },
         {
             key: "5",
             title: "CCY1",
-            className: "ff-poppins fw-bold"
+            dataIndex: 'ccy1',
+            className: "ff-poppins fw-bold",
         },
         {
             key: "6",
             title: "Amount",
-            className: "ff-poppins fw-bold"
+            dataIndex: 'amount',
+            className: "ff-poppins fw-bold",
         },
         {
             key: "7",
-            title: "Amount",
-            className: "ff-poppins fw-bold"
+            title: "Rate",
+            dataIndex: 'rate',
+            className: "ff-poppins fw-bold",
         },
         {
             key: "8",
-            title: "Rate",
-            className: "ff-poppins fw-bold"
+            title: "CCY2",
+            dataIndex: 'ccy2',
+            className: "ff-poppins fw-bold",
         },
         {
             key: "9",
-            title: "CCY2",
-            className: "ff-poppins fw-bold"
+            title: "Amount",
+            dataIndex: 'amount2',
+            className: "ff-poppins fw-bold",
         },
         {
             key: "10",
-            title: "Amount",
-            className: "ff-poppins fw-bold"
+            title: "Time",
+            dataIndex: 'time',
+            className: "ff-poppins fw-bold",
         },
         {
             key: "11",
-            title: "Time",
-            className: "ff-poppins fw-bold"
+            title: "LC No.",
+            dataIndex: 'lcNo',
+            className: "ff-poppins fw-bold",
         },
         {
             key: "12",
-            title: "LC No.",
-            className: "ff-poppins fw-bold"
+            title: "Account No.",
+            dataIndex: 'accountNo',
+            className: "ff-poppins fw-bold",
         },
         {
             key: "13",
-            title: "Account No.",
-            className: "ff-poppins fw-bold"
+            title: "Comment",
+            dataIndex: 'comment',
+            className: "comment-class ",
         },
         {
             key: "14",
-            title: "Comment"
+            title: "Status",
+            dataIndex: 'status',
+            className: "ff-poppins fw-bold",
         },
         {
             key: "15",
-            title: "Status",
-            className: "ff-poppins fw-bold"
+            title: "Chat",
+            dataIndex: 'chat',
+            className: "comment-class ",
         },
     ]
 
     return (
         <>
             <div className='box-content-wrapper'>
-                <GlobalTable bordered={false} prefixCls="TXNSummary_Table" columns={columns} />
+                <GlobalTable dataSource={tableData} bordered={false} prefixCls="TXNSummary_Table" columns={columns} />
             </div>
         </>
     )
