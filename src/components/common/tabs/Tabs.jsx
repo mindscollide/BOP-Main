8e7f80ebea3cf4e1879a7b20dbf8e8a6fea2566e
Owais Wajid Khan <owaiswajidkhan@gmail.com>
2024-08-27 17:03:47 +0500

    BM Owais

--- src/components/common/tabs/Tabs.jsx
+++ src/components/common/tabs/Tabs.jsx
@@ -2,20 +2,35 @@ import React from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import "./Tabs.css"
+import CustomButton from '../globalButton/button';
 
 const GlobalTabs = ({ tabs, defaultActiveKey = '0', tabClass }) => {
     return (
-        <Tabs
-            defaultActiveKey={defaultActiveKey}
-            id="uncontrolled-tab-example"
-            className={`${tabClass}`}
-        >
-            {tabs.map((tab, index) => (
-                <Tab eventKey={index.toString()} title={tab.title} key={index}>
-                    {tab.content}
-                </Tab>
-            ))}
-        </Tabs>
+        <>
+            <Tabs
+                defaultActiveKey={defaultActiveKey}
+                id="uncontrolled-tab-example"
+                className={`${tabClass}`}
+            >
+                {tabs.map((tab, index) => (
+                    <Tab eventKey={index.toString()} title={tab.title} key={index}>
+                        {tab.content}
+                    </Tab>
+                ))}
+            </Tabs>
+            <div className="filter-export-wrapper ms-auto">
+                <div className="d-flex align-items-center">
+                    <div className="nop-hd-container">
+                        <div className="d-flex align-items-center">
+                            <span className="hd-txt me-3">NOP (US$)</span>
+                            <span className="hd-cr me-2">46,999</span>
+                            <CustomButton applyClass={"NOP-button"} value="+" />
+                            <CustomButton applyClass={"Export-button"} value="Export" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
--- src/container/pages/mainTreasury/tabsContent/liveRates/blotter/blotterHeader/BlotterHeader.jsx
+++ src/container/pages/mainTreasury/tabsContent/liveRates/blotter/blotterHeader/BlotterHeader.jsx
@@ -14,21 +14,7 @@ const BlotterHeader = () => {
 
     return (
         <div className="box-header">
-            <div className="d-flex align-items-center">
-                <GlobalTabs tabClass="buttonClassTab" tabs={tabsData} defaultActiveKey={"0"} />
-                <div className="filter-export-wrapper ms-auto">
-                    <div className="d-flex align-items-center">
-                        <div className="nop-hd-container">
-                            <div className="d-flex align-items-center">
-                                <span className="hd-txt me-3">NOP (US$)</span>
-                                <span className="hd-cr me-2">46,999</span>
-                                <CustomButton applyClass={"NOP-button"} value="+" />
-                                <CustomButton applyClass={"Export-button"} value="Export" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <GlobalTabs tabClass="buttonClassTab" tabs={tabsData} defaultActiveKey={"0"} />
         </div>
     );
 }
