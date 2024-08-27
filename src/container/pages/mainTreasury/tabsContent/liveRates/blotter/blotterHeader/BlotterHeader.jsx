a61d8f83b2d8da5029ef74d8d94334740768e047
Owais Wajid Khan <owaiswajidkhan@gmail.com>
2024-08-27 17:09:12 +0500

    F

--- src/container/pages/mainTreasury/tabsContent/liveRates/blotter/blotterHeader/BlotterHeader.jsx
+++ src/container/pages/mainTreasury/tabsContent/liveRates/blotter/blotterHeader/BlotterHeader.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./BlotterHeader.css"
 import GlobalTabs from '../../../../../../../components/common/tabs/Tabs';
 import TXNSummary from '../txnSummary/TXNSummary';
@@ -7,27 +7,48 @@ import CustomButton from '../../../../../../../components/common/globalButton/bu
 import NopModal from '../nopModal/NopModal';
 
 const BlotterHeader = () => {
+
+    const [openNopModal, setOpenNopModal] = useState(false);
+
     const tabsData = [
         { title: "TXN Summary", content: <TXNSummary /> },
         { title: "Outstanding Deals", content: <OutstandingDeals /> },
     ];
 
+    const onClickNopModal = () => {
+        setOpenNopModal(true);
+    };
+
     return (
-        <div className="box-header">
-            <GlobalTabs tabClass="buttonClassTab" tabs={tabsData} defaultActiveKey={"0"} />
-            <div className="filter-export-wrapper ms-auto">
-                <div className="d-flex align-items-center">
-                    <div className="nop-hd-container">
-                        <div className="d-flex align-items-center">
-                            <span className="hd-txt me-3">NOP (US$)</span>
-                            <span className="hd-cr me-2">46,999</span>
-                            <CustomButton applyClass={"NOP-button"} value="+" />
-                            <CustomButton applyClass={"Export-button"} value="Export" />
+        <>
+            <div className="box-header">
+                <GlobalTabs tabClass="buttonClassTab" tabs={tabsData} defaultActiveKey={"0"} />
+                <div className="filter-export-wrapper ms-auto">
+                    <div className="d-flex align-items-center">
+                        <div className="nop-hd-container">
+                            <div className="d-flex align-items-center">
+                                <span className="hd-txt me-3">NOP (US$)</span>
+                                <span className="hd-cr me-2">46,999</span>
+                                <CustomButton
+                                    applyClass={"NOP-button"}
+                                    value="+"
+                                    onClick={onClickNopModal}
+                                />
+                                <CustomButton applyClass={"Export-button"} value="Export" />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+            {
+                openNopModal ? (
+                    <NopModal
+                        openNopModal={openNopModal}
+                        setOpenNopModal={setOpenNopModal}
+                    />
+                ) : null
+            }
+        </>
     );
 }
 
