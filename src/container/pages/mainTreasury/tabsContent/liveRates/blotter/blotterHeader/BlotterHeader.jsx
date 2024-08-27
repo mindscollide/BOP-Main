9460425e796dee617197622ac073d70f272e6432
aunnaqvi <aunnaqvi747@gmail.com>
2024-08-27 17:04:24 +0500

    code push
    code push

--- src/container/pages/mainTreasury/tabsContent/liveRates/blotter/blotterHeader/BlotterHeader.jsx
+++ src/container/pages/mainTreasury/tabsContent/liveRates/blotter/blotterHeader/BlotterHeader.jsx
@@ -1,36 +1,58 @@
-import React from 'react'
-import "./BlotterHeader.css"
-import GlobalTabs from '../../../../../../../components/common/tabs/Tabs';
-import TXNSummary from '../txnSummary/TXNSummary';
-import OutstandingDeals from '../outstandingDeals/OutstandingDeals';
-import CustomButton from '../../../../../../../components/common/globalButton/button';
-import NopModal from '../nopModal/NopModal';
+import React, { useState } from "react";
+import "./BlotterHeader.css";
+import GlobalTabs from "../../../../../../../components/common/tabs/Tabs";
+import TXNSummary from "../txnSummary/TXNSummary";
+import OutstandingDeals from "../outstandingDeals/OutstandingDeals";
+import CustomButton from "../../../../../../../components/common/globalButton/button";
+import NopModal from "../nopModal/NopModal";
 
 const BlotterHeader = () => {
-    const tabsData = [
-        { title: "TXN Summary", content: <TXNSummary /> },
-        { title: "Outstanding Deals", content: <OutstandingDeals /> },
-    ];
+  const [openNopModal, setOpenNopModal] = useState(false);
 
-    return (
-        <div className="box-header">
+  const onClickNopModal = () => {
+    setOpenNopModal(true);
+  };
+
+  const tabsData = [
+    { title: "TXN Summary", content: <TXNSummary /> },
+    { title: "Outstanding Deals", content: <OutstandingDeals /> },
+  ];
+
+  return (
+    <>
+      <div className="box-header">
+        <div className="d-flex align-items-center">
+          <GlobalTabs
+            tabClass="buttonClassTab"
+            tabs={tabsData}
+            defaultActiveKey={"0"}
+          />
+          <div className="filter-export-wrapper ms-auto">
             <div className="d-flex align-items-center">
-                <GlobalTabs tabClass="buttonClassTab" tabs={tabsData} defaultActiveKey={"0"} />
-                <div className="filter-export-wrapper ms-auto">
-                    <div className="d-flex align-items-center">
-                        <div className="nop-hd-container">
-                            <div className="d-flex align-items-center">
-                                <span className="hd-txt me-3">NOP (US$)</span>
-                                <span className="hd-cr me-2">46,999</span>
-                                <CustomButton applyClass={"NOP-button"} value="+" />
-                                <CustomButton applyClass={"Export-button"} value="Export" />
-                            </div>
-                        </div>
-                    </div>
+              <div className="nop-hd-container">
+                <div className="d-flex align-items-center">
+                  <span className="hd-txt me-3">NOP (US$)</span>
+                  <span className="hd-cr me-2">46,999</span>
+                  <CustomButton
+                    applyClass={"NOP-button"}
+                    value="+"
+                    onClick={onClickNopModal}
+                  />
+                  <CustomButton applyClass={"Export-button"} value="Export" />
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    );
-}
+      </div>
+      {openNopModal ? (
+        <NopModal
+          openNopModal={openNopModal}
+          setOpenNopModal={setOpenNopModal}
+        />
+      ) : null}
+    </>
+  );
+};
 
-export default BlotterHeader
+export default BlotterHeader;
