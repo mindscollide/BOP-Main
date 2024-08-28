55fe7445caaa171dbcaefa43d025ab3b446ef5d2
Owais Wajid Khan <owaiswajidkhan@gmail.com>
2024-08-28 13:53:36 +0500

    Merge remote-tracking branch 'origin/BM-AunBranch' into BM_OwaisBranch

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,6 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "@fontsource/roboto/900.css";
-import CalculatorFxDiscounting from "./components/features/calculatorFxDiscounting/CalculatorFxDiscounting";
 import MainCalculator from "./container/pages/mainCalculator/MainCalculator";
 
 function App() {
---
+++ src/components/utils/generateData.js
@@ -0,0 +1,128 @@
+let Data = {
+  tenors: [
+    {
+      tenorID: 1,
+      tenorName: "1 Month",
+      tenorDays: 30,
+    },
+    {
+      tenorID: 2,
+      tenorName: "3 Months",
+      tenorDays: 90,
+    },
+    {
+      tenorID: 3,
+      tenorName: "6 Months",
+      tenorDays: 180,
+    },
+    {
+      tenorID: 4,
+      tenorName: "1 Year",
+      tenorDays: 365,
+    },
+  ],
+  instruments: [
+    {
+      instrumentID: 1,
+      instrumentName: "Instrument A",
+    },
+    {
+      instrumentID: 2,
+      instrumentName: "Instrument B",
+    },
+    {
+      instrumentID: 3,
+      instrumentName: "Instrument C",
+    },
+  ],
+  forwardRates: [
+    {
+      tenorID: 1,
+      instrumentID: 1,
+      bid: 1.25,
+      ask: 1.3,
+    },
+    {
+      tenorID: 2,
+      instrumentID: 2,
+      bid: 1.35,
+      ask: 1.4,
+    },
+    {
+      tenorID: 3,
+      instrumentID: 3,
+      bid: 1.45,
+      ask: 1.5,
+    },
+  ],
+  discountRates: [
+    {
+      tenorID: 1,
+      instrumentID: 1,
+      rate: 0.02,
+    },
+    {
+      tenorID: 2,
+      instrumentID: 2,
+      rate: 0.025,
+    },
+    {
+      tenorID: 3,
+      instrumentID: 3,
+      rate: 0.03,
+    },
+  ],
+};
+export const generateData = (columnValue) => {
+  let discountRates = [];
+  let forwardsRates = [];
+  if (columnValue === 1) {
+    Data.discountRates.map((discValue, index) => {
+      let findTenorName = Data.tenors.find(
+        (tenorsData) => tenorsData.tenorID === discValue.tenorID
+      );
+      let findInstrumentName = Data.instruments.find(
+        (insturmentData) =>
+          insturmentData.instrumentID === discValue.instrumentID
+      );
+
+      const discountRateValue = {
+        key: `index ${index + 1}`,
+        Tenor: findTenorName ? findTenorName.tenorName : "",
+        tenorDays: findTenorName ? findTenorName.tenorDays : "",
+        instrumentName: findInstrumentName
+          ? findInstrumentName.instrumentName
+          : "",
+        value: discValue.rate,
+      };
+
+      discountRates.push(discountRateValue);
+    });
+  } else {
+    Data.forwardRates.map((forwData, index) => {
+      let findTenorName = Data.tenors.find(
+        (tenorsData) => tenorsData.tenorID === forwData.tenorID
+      );
+      let findInstrumentName = Data.instruments.find(
+        (insturmentData) =>
+          insturmentData.instrumentID === forwData.instrumentID
+      );
+
+      const forwardRateData = {
+        key: `index ${index + 1}`,
+        Tenor: findTenorName ? findTenorName.tenorName : "",
+        tenorDays: findTenorName ? findTenorName.tenorDays : "",
+        instrumentName: findInstrumentName
+          ? findInstrumentName.instrumentName
+          : "",
+        ask: forwData.ask,
+        bid: forwData.bid,
+      };
+
+      forwardsRates.push(forwardRateData);
+    });
+  }
+
+  // Return the result as an object or array if needed
+  return { discountRates, forwardsRates };
+};
