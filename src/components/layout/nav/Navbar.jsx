7c08b33f977f673a383158f456854021cf037f62
Ali Raza Mamdani <Ali.mamdani@hotmail.com>
2025-04-04 17:19:58 +0500

    Update Navbar.jsx

--- src/components/layout/nav/Navbar.jsx
+++ src/components/layout/nav/Navbar.jsx
@@ -101,7 +101,7 @@ const GlobalNavbar = () => {
                       </Suspense>
                     )}
                     {location.pathname.includes("treasury") &&
-                    (shouldIncludeDealer || shouldIncludeTreasury) ? (
+                    (shouldIncludeDealer || shouldIncludeTreasury || shouldIncludeCorporate) ? (
                       <CustomButton
                         applyClass="calcBtn"
                         value="Calculators"
