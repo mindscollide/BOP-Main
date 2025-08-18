752bfb98778785075aa53b438623625fb93bfcd2
Yunus Mustafa Zanzibarwala <85278320+yunuszanzi@users.noreply.github.com>
2025-08-18 12:48:11 +0500

    code push

--- src/components/features/blotter/mailModal/MailModal.jsx
+++ src/components/features/blotter/mailModal/MailModal.jsx
@@ -53,18 +53,22 @@ const MailModal = ({ openMailModal, setOpenMailModal }) => {
       }
     }
   }, [GetUsersEmail]);
+
   const onCloseModal = () => {
     setOpenMailModal(false);
   };
+
   console.log(emailOptions, "emailListemailList");
   const handleSelectUsers = (values) => {
     console.log(values, "selectedUser");
     setSelectedEmails(values);
   };
+
   const handleAddUsers = () => {
     setSendEmails([...sendEmails, ...selectedEmails]);
     setSelectedEmails([]);
   };
+
   const handleRemoveEmail = (value) => {
     console.log({ value }, "removedEmail");
     setSendEmails((prev) =>
@@ -88,6 +92,7 @@ const MailModal = ({ openMailModal, setOpenMailModal }) => {
       setIsExcel(false);
     }
   };
+
   const handleSendEmail = () => {
     console.log("handleSendEmail click", userRole, sendEmails);
     if (Number(userRole) === 9) {
@@ -113,6 +118,7 @@ const MailModal = ({ openMailModal, setOpenMailModal }) => {
       }
     }
     if (Number(userRole) === 8) {
+      //send email to treasury
       try {
         const Data = {
           Emails: sendEmails.map((e) => ({ Email: e.email })),
@@ -121,7 +127,7 @@ const MailModal = ({ openMailModal, setOpenMailModal }) => {
           MessageBody: message,
         };
         // console.log(Data, "DataDataData");
-        //send email to treasury
+
         dispatch(
           EmailBlotterTransactionDetailsForTreasuryAPI({
             navigate,
@@ -160,14 +166,6 @@ const MailModal = ({ openMailModal, setOpenMailModal }) => {
   const handleSendMessage = (event) => {
     const { name, value } = event.target;
     setMessage(value);
-    //   if (name === "message") {
-    //     const regex = /^[0-9]*$/;
-    //     if (regex.test(value)) {
-    //       setLcNumberData(value);
-    //     }
-    //   } else {
-    //     setLcNumberData(value);
-    //   }
   };
   return (
     <>
