f66936aff6134ca7aed0af490475049690d0d095
Ali Raza Mamdani <Ali.mamdani@hotmail.com>
2026-03-26 11:55:05 +0500

    Update TXNSummary.jsx

--- src/components/features/blotter/txnSummary/TXNSummary.jsx
+++ src/components/features/blotter/txnSummary/TXNSummary.jsx
@@ -1020,11 +1020,11 @@ const TXNSummary = () => {
                   className={"bg-none"}
                   onClick={handleTransactionModal}
                 />
-                <CustomButton
+                {/* <CustomButton
                   icon={<img src={printImage} alt='PDF Icon' />}
                   className={"bg-none"}
                   onClick={() => setExportButton(false)}
-                />
+                /> */}
               </div>
             }
             trigger='click'
